33da275323f43e41254267f4a0d6cc06838c7f76
soushi888 <sacha.pignot@protonmail.com>
2022-02-13 02:52:31 -0500

    update

--- youtube.cpp
+++ youtube.cpp
@@ -1,15 +1,31 @@
 #include <cstdio>
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
 
+int ask_pizza() {
+  int slices;
+  printf("How many slices of pizza do you want ?: ");
+  cin >> slices;
+
+  return slices;
+}
+
 int count_pizza(int num) {
-  cout << "You have " << num << " slices of pizza." << endl;
-  printf("You have %i pizza slices.\n", num);
-  return EXIT_SUCCESS;
+  double price = 1.25;
+  double total = num * price;
+  cout << "You have now " << num << " slices of pizza." << endl;
+  printf("The total cost is %f$.\n", total);
+
+  return total;
 }
 
 int main() {
-  count_pizza(5);
+  char hello[] = "Hello customer !\n";
+  cout << hello;
+  int slices = ask_pizza();
+  count_pizza(slices);
+  
   return EXIT_SUCCESS;
 }
