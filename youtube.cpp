ba6918755ba006228368390f20250fd0662f017b
soushi888 <sacha.pignot@protonmail.com>
2022-02-13 02:28:38 -0500

    update

--- youtube.cpp
+++ youtube.cpp
@@ -1,12 +1,15 @@
+#include <cstdio>
 #include <iostream>
 using std::cout;
+using std::endl;
 
-int say_hi() {
-  cout << "Hello World\n";
+int count_pizza(int num) {
+  cout << "You have " << num << " slices of pizza." << endl;
+  printf("You have %i pizza slices.\n", num);
   return EXIT_SUCCESS;
 }
 
 int main() {
-  say_hi();
+  count_pizza(5);
   return EXIT_SUCCESS;
 }
